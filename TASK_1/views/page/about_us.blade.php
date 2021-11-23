fbd77fe363b1d08d59f162271d3c377a558f9c66
Md. Shafiul Alam <67713577+shafi201104@users.noreply.github.com>
2021-11-23 22:56:25 +0600

    Add files via upload

---
+++ TASK_1/views/page/about_us.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+@section('content')
+    <br><br><br><h1>About Page</h1>
+
+@endsection
---
+++ TASK_1/views/page/product.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+@section('content')
+    <br><br><br><h1>Product / Service</h1>
+
+@endsection
